75d4111edbbc43ec7af935a2780b468a47d6a3b9
byeimhi <byeimhi@gmail.com>
2026-09-21 12:52:09 -0400

    Initial PlayerController commit
    just part of the move function

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,6 +3,10 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+
+    // Fields
+    Vector2 moveDirection;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,11 +16,13 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 currentPosition = transform.position;
+
+        currentPosition += 5.0f* moveDirection * Time.deltaTime;
     }
 
     public void move(InputAction.CallbackContext context)
     {
-        
+        moveDirection = context.ReadValue<Vector2>();
     }
 }
