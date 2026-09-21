c618e47dd88dafb2772f208c61fdf99eb137779c
byeimhi <byeimhi@gmail.com>
2026-09-21 16:26:02 -0400

    PlayerController set up

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,19 +3,33 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.Controls;
+using UnityEngine.UIElements;
 using static UnityEngine.RuleTile.TilingRuleOutput;
+
+enum PlayerState
+{
+    Moving,
+    Interacting,
+    Paused
+}
 
 public class PlayerController : MonoBehaviour
 {
+    // References
+    [SerializeField] PlayerInteraction interactRef;
 
     // Fields
-    string direction;
-    Vector2 currentPosition;
+    int HP; // Player Hit Points
+    int MP; // Player Magic Points
+    string direction; // Direction
+    Vector2 currentPosition; // Current Position (to be moved into)
+    bool idle; // Is the player idle?
+    PlayerState pSt; // Player state (for the state machine)
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        direction = "up";
     }
 
     // Update is called once per frame
@@ -23,12 +37,39 @@ public class PlayerController : MonoBehaviour
     {
         // Movement
         transform.position = currentPosition;
+        CheckRotation(direction);
+    }
+
+    /// <summary>
+    /// Rotates player based on direction
+    /// </summary>
+    /// <param name="d">Direction</param>
+    void CheckRotation(string d)
+    {
+        switch (d)
+        {
+            case "up":
+                transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+                break;
+
+            case "down":
+                transform.rotation = Quaternion.Euler(0f, 0f, 180f);
+                break;
+
+            case "left":
+                transform.rotation = Quaternion.Euler(0f, 0f, 90f);
+                break;
+
+            case "right":
+                transform.rotation = Quaternion.Euler(0f, 0f, -90f);
+                break;
+        }
     }
 
     // Movement Callbacks
     public void MoveUp(InputAction.CallbackContext context)
     {
-        if (context.started)
+        if (context.performed)
         {
             direction = "up";
             currentPosition = (Vector2)transform.position + new Vector2(0f, 1.0f);
@@ -37,7 +78,7 @@ public class PlayerController : MonoBehaviour
 
     public void MoveDown(InputAction.CallbackContext context)
     {
-        if (context.started)
+        if (context.performed)
         {
             direction = "down";
             currentPosition = (Vector2)transform.position + new Vector2(0f, -1.0f);
@@ -46,7 +87,7 @@ public class PlayerController : MonoBehaviour
 
     public void MoveLeft(InputAction.CallbackContext context)
     {
-        if (context.started)
+        if (context.performed)
         {
             direction = "left";
             currentPosition = (Vector2)transform.position + new Vector2(-1.0f, 0f);
@@ -55,7 +96,7 @@ public class PlayerController : MonoBehaviour
 
     public void MoveRight(InputAction.CallbackContext context)
     {
-        if (context.started)
+        if (context.performed)
         {
             direction = "right";
             currentPosition = (Vector2)transform.position + new Vector2(1.0f, 0f);
